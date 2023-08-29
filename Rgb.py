d54338f986af4bd3d9431652856227e5a47083a3
lkelley36 <lkelley36@charlottesvilleschools.org>
2023-08-29 12:46:42 -0400

    fixed neopixel pins

--- Rgb.py
+++ Rgb.py
@@ -6,10 +6,10 @@ import board
 from rainbowio import colorwheel
 import neopixel
 
-NUMPIXELS = 16  # Update this to match the number of LEDs.
+NUMPIXELS = 1 # Update this to match the number of LEDs.
 SPEED = 0.05  # Increase to slow down the rainbow. Decrease to speed it up.
 BRIGHTNESS = 0.2  # A number between 0.0 and 1.0, where 0.0 is off, and 1.0 is max.
-PIN = board.A3  # This is the default pin on the 5x5 NeoPixel Grid BFF.
+PIN = board.NEOPIXEL  # This is the default pin on the 5x5 NeoPixel Grid BFF.
 
 pixels = neopixel.NeoPixel(PIN, NUMPIXELS, brightness=BRIGHTNESS, auto_write=False)
 
